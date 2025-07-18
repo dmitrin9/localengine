ba1ec2cea3e09a6c1dd4e71bd4d72291c7fb0ac1
Dmitri <dmitya@yahoo.com>
2025-07-18 13:49:35 +0100

    Refined interface.

--- app.py
+++ app.py
@@ -1,14 +1,25 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for, redirect
+import base64
 import search as searchRanking
 
 app = Flask(__name__)
 
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "POST":
+        search = request.form.get("search")
+        byte = search.encode("utf-8")
+        encoded = base64.b64encode(byte)
+        url_endpoint = url_for("search", terms=encoded.decode("utf-8"))
+        return redirect(url_endpoint)
+
+    return render_template("index.html")
+
 @app.route("/search/<terms>")
 def search(terms):
-    splittedTerms = terms.split("+")
-    print("TERMS ", splittedTerms)
+    termsEncode = base64.b64decode(terms.encode("utf-8"))
+    splittedTerms = termsEncode.decode("utf-8").split(" ")
     rank = searchRanking.search(splittedTerms)
-    print(rank)
     return render_template("results.html", terms=splittedTerms, rank=rank)
 
 if __name__ == "__main__":
